466de48b0bce6eb27edcdddd8884035569fa041d
Brendon Edmiston <Brendon_Edmiston@reyrey.com>
2018-01-10 10:08:57 -0600

    Move Tally UI to Tally component

---
+++ src/components/Results/Tally.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class Tally extends React.Component {
+  getPair() {
+    return this.props.pair || [];
+  }
+
+  getVotes(entry) {
+    if (this.props.tally && this.props.tally.has(entry)) {
+      return this.props.tally.get(entry);
+    }
+    return 0;
+  }
+
+  render() {
+    return (
+      <div className="results">
+        <div className="tally">
+          {this.getPair().map(entry =>
+            <div key={entry} className="entry">
+              <h1>{entry}</h1>
+              <div className="voteCount">
+                {this.getVotes(entry)}
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="management">
+          <button className="next"
+            onClick={this.props.next} />
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Tally;
